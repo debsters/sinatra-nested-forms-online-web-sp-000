a7e3a04550ef36f33f1ff84e13a3192c77edaf73
Deborah Barcenas <deborah.barcenas@gmail.com>
2020-06-08 18:43:06 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  
+  attr_reader :name, :type, :booty_attributes
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty_attributes = params[:booty_attributes]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+  
 end
